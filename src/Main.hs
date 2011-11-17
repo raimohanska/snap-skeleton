10d45dcded2518d5aee8e62f9345aa5e3e6d0317
Juha Paananen <juha.paananen@reaktor.fi>
2011-11-17 23:47:19 +0200

    Use UTF-8

--- src/Main.hs
+++ src/Main.hs
@@ -6,13 +6,15 @@ import           Control.Monad.Trans(liftIO)
 import           Data.ByteString (ByteString)
 import           Snap.Core
 import qualified Data.ByteString.Lazy.Char8 as L8
+import qualified Data.Text.Lazy.Encoding as E
+import qualified Data.Text.Lazy as T
 
 lol :: Snap()
 lol = do 
-    reqBody <- liftM L8.unpack getRequestBody
+    reqBody <- liftM (T.unpack . E.decodeUtf8) getRequestBody
     liftIO $ putStrLn $ "Received " ++ reqBody
     let reply = "You got lolld"
-    writeLBS $ L8.pack $ reply  
+    writeLBS $ (E.encodeUtf8 . T.pack) $ reply  
 
 site :: Snap ()
 site = route [ ("/", lol) ]
