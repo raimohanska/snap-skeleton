e50dc01e30a5892cd0977fb27baa57e2f9fe299e
Juha Paananen <juha.paananen@reaktor.fi>
2011-11-20 00:15:05 +0200

    cleanup

--- src/Examples/Restful.hs
+++ src/Examples/Restful.hs
@@ -6,11 +6,6 @@ import           Control.Monad
 import           Control.Monad.Trans(liftIO)
 import           Data.ByteString (ByteString)
 import           Snap.Core
-import qualified Data.ByteString.Lazy.Char8 as L8
-import qualified Data.Text.Encoding as ES
-import qualified Data.Text as TS
-import qualified Data.Text.Lazy.Encoding as E
-import qualified Data.Text.Lazy as T
 import           Data.Typeable
 import           Data.Data
 import           Text.JSON.Generic
